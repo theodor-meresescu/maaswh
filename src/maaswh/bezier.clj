e1c74ace5d2fde9008975c7c7ba0cc401d3b7261
Theodor Meresescu <theodor.meresescu@flexiana.com>
2024-01-22 22:21:33 +0200

    refactor smooth bezier

--- src/maaswh/bezier.clj
+++ src/maaswh/bezier.clj
@@ -2,29 +2,6 @@
   (:require
     [maaswh.vector3 :as v3]))
 
-(defn ^{:superseded-by "bernstein"} quadratic
-  [points t]
-  (let [u (- 1 t)]
-    (v3/add (v3/scale-by (* u u) (first points))
-            (v3/scale-by (* 2 u t) (second points))
-            (v3/scale-by (* t t) (nth points 2)))))
-
-(defn ^{:superseded-by "bernstein"} cubic
-  [points t]
-  (let [u (- 1 t)]
-    (v3/add (v3/scale-by u (quadratic (take 3 points) t))
-            (v3/scale-by t (quadratic (rest points) t)))))
-
-(defn ^{:superseded-by "bernstein"} de-casteljau
-  [curve t]
-  (cond
-    (empty? curve) {:x 0 :y 0 :z 0}
-    (= (count curve) 1) (first curve)
-    (= (count curve) 2) (v3/lerp (first curve) (second curve) t)
-    (= (count curve) 3) (quadratic curve t)
-    (= (count curve) 4) (cubic curve t)
-    :else (recur (map #(cubic % t) (partition 4 1 curve)) t)))
-
 (defn factorial
   [n]
   (reduce * (range 1 (inc n))))
